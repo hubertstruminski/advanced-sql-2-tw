fb76e6ac18a9591e64fd79b12bc5ff997aa3b400
hubert.struminski <hubert.struminski@gmail.com>
2019-04-15 20:40:41 +0200

    add ER Diagram of Flight management system to the project structure

--- tables.sql
+++ tables.sql
@@ -16,8 +16,13 @@ CREATE TABLE seats (
 
 CREATE TABLE planes (
 	id integer UNIQUE PRIMARY KEY,
-	name VARCHAR(60) NOT NULL,
-	-------------------------- seats_id integer REFERENCES seats(id)
+	name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE planes_seats (
+	id integer UNIQUE PRIMARY KEY,
+	planes_id integer REFERENCES planes(id),
+	seats_id integer REFERENCES seats(id)
 );
 
 CREATE TABLE gates (
@@ -54,25 +59,25 @@ CREATE TABLE customers (
 CREATE TABLE employers (
 	id integer UNIQUE PRIMARY KEY,
 	profession VARCHAR(60) NOT NULL,
-	name VARCHAR(60) NOT NULL
+	name VARCHAR(60) NOT NULL,
 	surname VARCHAR(150) NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	sex VARCHAR(15) NOT NULL,
 	street VARCHAR(120) NOT NULL,
 	city VARCHAR(150) NOT NULL,
 	country VARCHAR(150) NOT NULL,
-	phone_number VARCHAR(30) NOT NULL
-	customers_id integer REFERENCES customers(id) -------------
+	phone_number VARCHAR(30) NOT NULL,
+	customers_id integer REFERENCES customers(id)
 );
 
 CREATE TABLE flights (
 	id integer UNIQUE PRIMARY KEY,
-	offers_id integer REFERENCES offers(id)
+	offers_id integer REFERENCES offers(id),
 	time_departure TIME NOT NULL,
 	time_arrival TIME NOT NULL,
 	data DATE NOT NULL,
 	flight_duration TIME NOT NULL,
-	delays_id integer REFERENCES delays(id)
+	delays_id integer REFERENCES delays(id),
 	gates_id integer REFERENCES gates(id)
 );
 
